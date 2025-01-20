ac8addd05ee4a1ddd306cac63843c262c5eb0d78
Steve McBride <github@madbox.co.uk>
2025-01-20 09:26:40 +0000

    Create rke2-m-n.sh
    new file

---
+++ rke2-m-n.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Environment prep.
+mkdir -p /etc/rancher/rke2
+#####
+# /etc/rancher/rke2/config.yaml
+
+cat << EOF > /etc/rancher/rke2/config.yaml
+token: c0k3z3r0-is-my-shared-secret
+server: https://172.31.16.245:9345
+tls-san:
+- rms.geekosoup.com
+EOF
+
+ls -l /etc/rancher/rke2/
+
+curl -sfL https://get.rke2.io | sh -
+systemctl enable rke2-server.service
+systemctl start rke2-server.service
+
+kubectl get nodes
+cat /var/lib/rancher/rke2/server/node-token
