c9759c57f18387355562f10f263b8b5e83329716
Steve McBride <github@madbox.co.uk>
2025-01-20 10:12:57 +0000

    Update rke2-m-n.sh
    new files

--- rke2-m-n.sh
+++ rke2-m-n.sh
@@ -9,14 +9,16 @@ cat << EOF > /etc/rancher/rke2/config.yaml
 token: c0k3z3r0-is-my-shared-secret
 server: https://172.31.16.245:9345
 tls-san:
+- 18.201.101.151
+- 172.31.16.245
 - rms.geekosoup.com
 EOF
 
 ls -l /etc/rancher/rke2/
 
 curl -sfL https://get.rke2.io | sh -
+
 systemctl enable rke2-server.service
 systemctl start rke2-server.service
 
-kubectl get nodes
 cat /var/lib/rancher/rke2/server/node-token
